9627ad8d1aed712bcfac16a0f157b64986adef47
Rohit Patil <rohitpatil1810@gmail.com>
2020-07-11 14:38:00 +0530

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Rohit Patil on 11/07/20.
+//  Copyright © 2020 Patil corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
